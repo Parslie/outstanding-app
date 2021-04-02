f302188a3a03398c3cf4302ce5b58d62f12dc8e9
Viktor Holta <viktor.holta@outlook.com>
2021-04-02 16:26:27 +0200

    NEW: init account settings view model

--- app/src/main/java/com/vikho305/isaho220/outstanding/ui/view/AccountSettingsActivity.kt
+++ app/src/main/java/com/vikho305/isaho220/outstanding/ui/view/AccountSettingsActivity.kt
@@ -1,16 +1,43 @@
 package com.vikho305.isaho220.outstanding.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.vikho305.isaho220.outstanding.R
 import com.vikho305.isaho220.outstanding.databinding.ActivityAccountSettingsBinding
+import com.vikho305.isaho220.outstanding.ui.viewmodel.AccountSettingsViewModel
+import com.vikho305.isaho220.outstanding.ui.viewmodel.ContextualViewModelFactory
+import com.vikho305.isaho220.outstanding.ui.viewmodel.MainViewModel
+import com.vikho305.isaho220.outstanding.util.Resource
 
 class AccountSettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAccountSettingsBinding
+    private lateinit var viewModel: AccountSettingsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_account_settings)
+
+        initViewModel()
+        setOnClickListeners()
+    }
+
+    private fun initViewModel() {
+        val viewModelFactory = ContextualViewModelFactory(this)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(AccountSettingsViewModel::class.java)
+    }
+
+    private fun setOnClickListeners() {
+        binding.accountSettingsPfpChooseBtn.setOnClickListener {
+            println("Clicked Choose Button")
+        }
+        binding.accountSettingsPfpResetBtn.setOnClickListener {
+            println("Clicked Reset Button")
+        }
+        binding.accountSettingsSaveBtn.setOnClickListener {
+            println("Clicked Save Button")
+        }
     }
 }
---
+++ app/src/main/java/com/vikho305/isaho220/outstanding/ui/viewmodel/AccountSettingsViewModel.kt
@@ -0,0 +1,65 @@
+package com.vikho305.isaho220.outstanding.ui.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.vikho305.isaho220.outstanding.data.repositories.UserRepository
+import com.vikho305.isaho220.outstanding.ui.base.BaseViewModel
+import com.vikho305.isaho220.outstanding.util.Resource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+
+class AccountSettingsViewModel(context: Context) : BaseViewModel() {
+    private val userRepository: UserRepository = UserRepository(context)
+    private val hasSetAccount:  MutableLiveData<Resource<Boolean>> = MutableLiveData()
+    private val hasSetProfile:  MutableLiveData<Resource<Boolean>> = MutableLiveData()
+
+    fun hasSetAccount(): LiveData<Resource<Boolean>> {
+        return hasSetAccount
+    }
+
+    fun hasSetProfile(): LiveData<Resource<Boolean>> {
+        return hasSetProfile
+    }
+
+    fun setAccount(username: String, email: String, password: String) {
+        val onSuccess: Consumer<String> = Consumer {
+            hasSetAccount.postValue(Resource.success(true))
+        }
+        val onError: Consumer<Throwable> = Consumer {
+            hasSetAccount.postValue(Resource.error(false))
+        }
+
+        hasSetAccount.postValue(Resource.loading(false))
+
+        if (password.isNotEmpty()) {
+            addDisposable(userRepository.setAccountDetails(username, email, password)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(onSuccess, onError))
+        } else {
+            addDisposable(userRepository.setAccountDetails(username, email)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(onSuccess, onError))
+        }
+    }
+
+    fun setProfile(pfp: String, description: String) {
+        val onSuccess: Consumer<String> = Consumer {
+            hasSetProfile.postValue(Resource.success(true))
+        }
+        val onError: Consumer<Throwable> = Consumer {
+            hasSetProfile.postValue(Resource.error(false))
+        }
+
+        hasSetProfile.postValue(Resource.loading(false))
+
+        // TODO: figure out what to do with profile colors
+        addDisposable(userRepository.setProfileDetails(pfp, description, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onSuccess, onError))
+    }
+}
