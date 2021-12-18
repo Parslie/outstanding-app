43df6da7ebbbcf5fba5a5b6cb7a7f074170a9e44
Viktor Holta <viktor.holta@outlook.com>
2021-12-18 14:12:59 +0100

    Start new android studio project

---
+++ app/src/main/java/com/tavro/parslie/outstanding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tavro.parslie.outstanding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/vikho305/isaho220/outstanding/util/Status.java
+++
@@ -1,7 +0,0 @@
-package com.vikho305.isaho220.outstanding.util;
-
-public enum Status {
-    SUCCESS,
-    ERROR,
-    LOADING
-}
